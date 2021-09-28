ff9b65a2df83d8d5fe94e63c8532aa20861ea6c2
Robert Klinger <robert@k1inger.de>
2021-09-28 15:13:37 +0200

    fix: removed pathHandling from TYPES

--- src/TYPES.ts
+++ src/TYPES.ts
@@ -1,6 +1,5 @@
 export const BUGFINDER_COMMITPATH_QUANTIFIER_SONARQUBE_TYPES = {
     sonarQubeConfig:    Symbol("SonarQubeConfig"),
     git:                Symbol("Git"),
-    pathsHandling:      Symbol("PathsHandling"),
     logger:             Symbol("Logger")
 }
